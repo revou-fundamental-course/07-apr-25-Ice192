88f8cf71c489fab4c0da93e5dbfa4162c73334c4
muhammadraisb2@gmail.com <muhammadraisb2@gmail.com>
2025-04-17 14:34:37 +0800

    autoslide & responsif

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
+// Daftar gambar yang akan ditampilkan dalam slideshow
+const imageList = [
+  'assets/hero1.png',
+  'assets/hero2.png',
+  'assets/hero3.png'
+];
+
+// Menyimpan indeks dan menampilkan gambar sesuai dengan id nya
+let currentIndex = 0;
+const slideshowImage = document.getElementById('slideshow-image');
+
+// Pengaturan agar gambar berganti
+setInterval(() => {
+  currentIndex = (currentIndex + 1) % imageList.length;
+  slideshowImage.src = imageList[currentIndex];
+}, 5000);
+
+// Pembuatan variable dengan mengambil dari id
 const form = document.getElementById("contact-form");
 const result = document.getElementById("result-display");
 
+// Mencegah form reload
 form.addEventListener("submit", function (e) {
-    e.preventDefault(); // Mencegah form reload
+    e.preventDefault(); 
     console.log("Form Submitted");
 
   const currentTime = new Date().toLocaleString();
@@ -11,6 +30,7 @@ form.addEventListener("submit", function (e) {
   const gender = form.elements["gender"].value;
   const message = form.elements["message"].value;
 
+  // Menampilkan hasil dengan InnerHTML
   result.innerHTML = `
     <p><strong>Current Time : </strong> ${currentTime}</p>
     <p><strong>Nama : </strong> ${name}</p>
